722f7f8b7161139e0722b921641926b5225ddc80
gopa810 <root@gopal.home.sk>
2014-10-27 10:48:17 +0100

    first commit

---
+++ AsciiTabGen/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AsciiTabGen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 128; i < 256; i+=16)
+            {
+                for (int j = 0; j < 16; j++)
+                {
+                    sb.AppendFormat("{0}: {1}  ", i + j, Convert.ToChar(i + j));
+                }
+                sb.AppendLine();
+            }
+
+            richTextBox1.Text = sb.ToString();
+        }
+    }
+}
